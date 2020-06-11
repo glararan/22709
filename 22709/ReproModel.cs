3c0c2cde169c6ad84e08a6e6dd55560adaeecc95
glararan <glararan@glararan.eu>
2020-06-11 10:53:25 +0200

    Upload

---
+++ 22709/ReproModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace repro
+{
+    public class ReproModel
+    {
+        [Required, Display(Name = "Password"), DataType(DataType.Password)]
+        [StringLength(100, MinimumLength = 6)]
+        public string Password { get; set; }
+
+        [Display(Name = "ConfirmPassword"), DataType(DataType.Password)]
+        [CompareProperty("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ 22709/ValidationContext.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace repro
+{
+    public class ValidationContext<T> where T : class
+    {
+        public T Model { get; private set; }
+
+        public EditContext EditContext { get; private set; }
+
+        public Dictionary<string, List<string>> Errors { get; private set; } = new Dictionary<string, List<string>>();
+
+        public ValidationContext(T model)
+        {
+            Model = model;
+
+            EditContext = new EditContext(Model);
+            EditContext.OnFieldChanged += EditContext_OnFieldChanged;
+        }
+
+        void EditContext_OnFieldChanged(object sender, FieldChangedEventArgs e)
+        {
+            PropertyInfo property = Model.GetType().GetProperty(e.FieldIdentifier.FieldName);
+
+            if (property is null)
+                return;
+
+            IEnumerable<ValidationAttribute> attributes = property.GetCustomAttributes<ValidationAttribute>();
+
+            List<string> errors = new List<string>();
+
+            if (Errors.ContainsKey(e.FieldIdentifier.FieldName))
+                Errors.Remove(e.FieldIdentifier.FieldName);
+
+            foreach (ValidationAttribute attribute in attributes)
+            {
+                if (!attribute.IsValid(property.GetValue(Model)))
+                    errors.Add(attribute.ErrorMessage);
+            }
+
+            if (errors.Count > 0)
+                Errors[e.FieldIdentifier.FieldName] = errors;
+        }
+    }
+}
